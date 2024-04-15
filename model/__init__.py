116cb76e1572591cc4c979ef2f5e95a59628bee6
pralhadshinde99 <pralhadshinde299@gmail.com>
2024-04-15 09:39:00 +0530

    First commit

---
+++ model/__init__.py
@@ -0,0 +1,6 @@
+from model.hospital_model import Hospital
+from model.technician_model import Technician
+from model.reporting_doctor_model import ReportingDoctor
+from model.patient_model import Patient
+from model.patient_file_model import PatientFile
+from model.document_model import Document
